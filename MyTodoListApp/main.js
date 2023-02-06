db444c7ff6f9c2c9f8b18abde94d3e027a5ed3b8
Mpho Dinala <79215926+Tuckker@users.noreply.github.com>
2023-02-06 12:50:16 +0200

    Adding files via upload

---
+++ MyTodoListApp/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load',() =>{
+        const form = document.querySelector('new-goal-form');
+        const input = document.querySelector('new-goal-input');
+        const list_e= documeny.querySelector('#goals');
+})

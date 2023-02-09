52fd845ffe26e117073c8782c5b9f456719c0522
Mpho Dinala <mphodinala1@gmail.com>
2023-02-09 09:00:39 +0200

    adding function that ensures that goal is filled in

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
+//creating the elememts
 window.addEventListener('load',() =>{
         const form = document.querySelector('new-goal-form');
         const input = document.querySelector('new-goal-input');
-        const list_e= documeny.querySelector('#goals');
+        const listElement= documeny.querySelector('#goals');
+
+        console.log(form);
+
+
+
+        //this functions ensures that a goal is filled in
+form.addEventListener('submit',(e)=>{
+
+        e.preventDefault();
+
+        const goal= input.value;
+
+        if(!goal){
+                alert("Please fill out the task");
+                return;
+        }
+        
+
 })
+        
+
+        
+})
+

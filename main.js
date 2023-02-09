6ef8bec9ec23b3dc7b78d9eded5de7b17b16bb0b
Mpho Dinala <mphodinala1@gmail.com>
2023-02-09 09:59:39 +0200

    adding delete and edit button elements

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ form.addEventListener('submit',(e) =>{
         const goal= input.value;
 
         if(!goal){
-                alert("Please fill out the task");
+                alert("Please fill out the goal");
                 return;
         }
         
         //these elements will ensure  they get stored and shown on the app
         const goal_Element = document.createElement("div");
-        goal_Element.classList.add("goal");
+        goal_Element.classList.add("goals");
 
         const goal_content_element = document.createElement("div");
         goal_content_element.classList.add("content");
@@ -37,6 +37,18 @@ form.addEventListener('submit',(e) =>{
         goal_input_element.value =goal;
         goal_input_element.setAttribute("readonly","readonly");
         goal_content_element.appendChild(goal_input_element);
+
+        //here we will be adding the button elements
+
+        const goal_action_element = document.createElement("div");
+        goal_action_element>classList.add("action");
+
+        const goal_edit_element = document.createElement("button");
+        goal_edit_element.classList.add("edit");
+
+        const goal_delete_element = document.createElement("button");
+        goal_delete_element.classList.add("delete");
+
 
         listElement.appendChild(goal_Element);
 
